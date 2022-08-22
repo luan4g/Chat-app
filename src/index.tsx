6bfe6a493cc32347255d3bf62f36961f2e05986f
luan4g <luan.ferreira.mads@gmail.com>
2022-08-22 13:05:40 -0300

    create header&style

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,41 @@
 import {
   View,
-  Text
+  Text,
+  Image
 } from 'react-native'
-import styles from './styles/Chat'
+import { FontAwesome5, Entypo } from '@expo/vector-icons'
+
+import styles from './styles/Chat';
 
 const Chat = () => {
   return(
     <View style={styles.container}>
-      <Text>Foto</Text>
-      <Text>Nome</Text>
-      <Text>Msg 1</Text>
-      <Text>Msg 2</Text>
-      <Text>Escrever</Text>
-      <Text>Enviar</Text>
+      <View style={styles.header}>
+        <FontAwesome5 name="arrow-left" size={20} color="#242424" />
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            marginLeft: 16,
+            marginRight: 8,
+          }}
+        >
+          <Image 
+            source={{uri: "https://64.media.tumblr.com/e7b8e675ab152bd9e1f6807ea291b7da/df220733d8dd576b-d2/s540x810/d4a6e37d6876efe363b1f3c24967ea27d49f1907.pnj"}}
+            style={styles.userPhoto}
+          />  
+          <View style={styles.userDotStatus}/>
+        </View>
+        <View>
+          <Text style={styles.userName} >Arataki Itto</Text>
+          <Text style={styles.userStatus} >Online</Text>
+        </View>
+
+        <View style={styles.userCall}>
+          <Entypo name="video-camera" size={20} color="#ddd" />
+          <FontAwesome5 name="phone-alt" size={20} color="#ddd" />
+        </View>
+      </View>
     </View>
   )
 }
--- src/styles/Chat.tsx
+++ src/styles/Chat.tsx
@@ -3,8 +3,51 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+  },
+
+  header: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    height: 80,
     alignItems: "center",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    width: "100%",
+  },
+
+  userPhoto: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+
+  userDotStatus: {
+    position: "absolute",
+    width: 12,
+    height: 12,
+    borderRadius: 20,
+    backgroundColor: "#00ff00",
+    borderWidth: 2,
+    borderColor: "#fff",
+    bottom: 0,
+    right: 0,
+  },
+
+  userName: {
+    fontSize: 16,
+  },
+
+  userStatus: {
+    marginTop: 4,
+    fontSize: 10,
+    color: "#242424"
+  },
+
+  userCall: {
+    flexDirection: "row",
+    marginLeft: "auto",
+    width: 100,
+    justifyContent: "space-around"
   }
 })
 
